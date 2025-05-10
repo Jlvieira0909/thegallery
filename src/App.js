b85dac80035422c5a518315ad9de5a1684d6fe14
Jlvieira0909 <lvjohn09@gmail.com>
2025-05-10 12:56:37 -0300

    "feat: created app"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="museumWall">
+      <img className="exhibition" alt="" src="/artCollection/Jordan.jpg"></img>
+      <button className="infoButton"></button>
+      <img
+        className="exhibition"
+        alt=""
+        src="/artCollection/Thanatos.jpg"
+      ></img>
+      <img className="exhibition" alt="" src="/artCollection/Group.jpg"></img>
+      <img className="exhibition" alt="" src="/artCollection/Curry.jpg"></img>
+      <img
+        className="exhibition"
+        alt=""
+        src="/artCollection/RipScrew.jpg"
+      ></img>
+      <img className="exhibition" alt="" src="/artCollection/Group2.jpg"></img>
+      <img
+        className="exhibition"
+        alt=""
+        src="/artCollection/AstroThunder.jpg"
+      ></img>
+      <img
+        className="exhibition"
+        alt=""
+        src="/artCollection/TheWeeknd.jpg"
+      ></img>
+      <img className="exhibition" alt="" src="/artCollection/Group4.jpg"></img>
+      <img className="exhibition" alt="" src="/artCollection/Meme.jpg"></img>
+      <img className="exhibition" alt="" src="/artCollection/Group3.jpg"></img>
+      <img
+        className="exhibition"
+        alt=""
+        src="/artCollection/HoustonFornication.jpg"
+      ></img>
     </div>
   );
 }
